493a2a2f36d9d94cb943890218782ab8d4ee5af6
czsquizer <smrcek.marek@gmail.com>
2021-07-14 16:40:19 +0200

    Version 1.0.0 (alpha)

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Squizer#3020'
+description 'Script that allows you to borrow vehicles.'
+version '1.0.0'
+
+client_scripts {
+    'warmenu.lua',
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
